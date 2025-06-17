b30ea15173b29b0def0ada8d27bf3cac5ef21461
AltheaZuleika <sadiconaltheazuleika@gmail.com>
2025-06-17 20:56:21 +0800

    feat: update database checkers with new ports and verify MongoDB/PostgreSQL connectivity.

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,10 +1,10 @@
 <?php
 
 $host = "host.docker.internal"; 
-$port = "5112";
+$port = "5555";
 $username = "user";
 $password = "password";
-$dbname = "mydatabase";
+$dbname = "adtask3_postgres";
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
